dcf393b5bb00036154d8950521cc59476de5f4eb
Alex <alexey-bycov@yandex.ru>
2019-04-03 14:58:03 +0700

    Kostyak of the igra)))

---
+++ Assets/Scripts/WorldPopulatingController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPopulatingController : MonoBehaviour
+{
+    private PlayerController player;
+    private float oldDistance;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        oldDistance = player.distance;
+    }
+    
+    void Update()
+    {
+        float distance = player.distance;
+        // Do stuff
+    }
+}
