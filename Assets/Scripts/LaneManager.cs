e5944189ac1f9821a067a3e97ebf494154e5142f
Synthwaver <makc-sam@mail.ru>
2019-04-07 15:30:19 +0700

    Add LaneManager

---
+++ Assets/Scripts/LaneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LaneManager
+{
+    private static float[] lanesXCoords = { -3, -1, 1, 3 };
+
+    public static float GetX(int lane) {
+        return lanesXCoords[lane];
+    }
+}
