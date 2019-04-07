7d2edca52ad47239f1bda8b7af7bfb299893366b
Alex <alexey-bycov@yandex.ru>
2019-04-07 15:04:37 +0700

    Add basic movement in straight forward direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float startSpeed = 15f;
+    [SerializeField] float acceleration = 0.1f;
+
     public float distance => transform.position.z;
+
+    private Rigidbody rb;
+    private bool isAlive;
+    private float currentSpeed;
+    // can be [0; 3]
+    private int lane = 3;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = startSpeed;
+        isAlive = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isAlive) return;
+
+        rb.position += new Vector3(0, 0, currentSpeed * Time.deltaTime);
+        currentSpeed += acceleration * Time.deltaTime;
+    }
+    
 }
