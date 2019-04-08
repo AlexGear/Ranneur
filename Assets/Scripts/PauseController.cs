31e01dc5ad08b9a2d4dd3733c55453018c03b3f6
Alex <alexey-bycov@yandex.ru>
2019-04-08 16:08:18 +0700

    Add 3d models

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -6,17 +6,6 @@ using UnityEngine.SceneManagement;
 public class PauseController : MonoBehaviour
 {
     [SerializeField] GameObject panel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ReloadLevel() {
         SetActivePanel();
