28f508f15fa1d7a8af31f318833fc2994256acca
Maxim190 <vbrc1212@yandex.ru>
2019-04-08 14:34:09 +0700

    Menu [2]

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReloadLevel() {
+        SetActivePanel();
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetActivePanel() {
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    public void ExitTheGame() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [SerializeField] GameObject textField;
+    private static Text scoreText;
+
+    private void Awake()
+    {
+        scoreText = textField.GetComponent<Text>();
+    }
+
+    private static int scoreValue = 0;
+    public static int ScoreValue{
+        get { return scoreValue; }
+        set {
+            scoreValue = value;
+            scoreText.text = scoreValue.ToString();
+        }
+    }
+}
